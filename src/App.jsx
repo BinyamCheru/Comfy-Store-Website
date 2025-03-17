5ef1a89ab6dbf3386f553e48e722835ec7b87f51
Bina <benjaminbinyam500@gmail.com>
2025-03-17 10:00:56 +0300

    Comfy-project setup - install all the necessary dependencies

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {
+  HomeLayout,
+  Landing,
+  Error,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  Register,
+  Login,
+  Checkout,
+  Orders,
+} from "./pages";
+
+function App() {
+  return <h1 className="bg-green-200">Hello</h1>;
+}
+
+export default App;
