4c5c56965b8a97e942cff2959c989857e60b1027
Bina <benjaminbinyam500@gmail.com>
2025-03-18 14:14:37 +0300

    Add FeaturedProducts section - format the price in dollars

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,4 @@
-import { Hero } from "../components";
+import { FeaturedProducts, Hero } from "../components";
 import { customFetch } from "../utils";
 
 const url = "/products?featured=true";
@@ -14,6 +14,7 @@ const Landing = () => {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   );
 };
