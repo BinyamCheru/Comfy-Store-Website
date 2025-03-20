58c5625aca0ccc5af881c02b729438b15dd33166
Bina <benjaminbinyam500@gmail.com>
2025-03-20 13:49:10 +0300

    Fix theme bu using the redux - userSlice

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,12 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const themes = {
+  winter: "winter",
+  dracula: "dracula",
+};
+
+const getThemeFromLocalStorage = () => {
+  const theme = localStorage.getItem("theme") || themes.winter;
+  document.documentElement.setAttribute("data-theme", theme);
+  return theme;
+};
+
 const initialState = {
   user: { username: "codding addict" },
-  theme: "dracula",
+  theme: getThemeFromLocalStorage(),
 };
 
 const userSlice = createSlice({
-  name: "user".initialState,
+  name: "user",
+  initialState,
   reducers: {
     loginUser: (state) => {
       console.log("login");
@@ -15,11 +27,13 @@ const userSlice = createSlice({
       console.log("logout");
     },
     toggleTheme: (state) => {
-      console.log("toggle theme");
+      const { dracula, winter } = themes;
+      state.theme = state.theme === dracula ? winter : dracula;
+      document.documentElement.setAttribute("data-theme", state.theme);
+      localStorage.setItem("theme", state.theme);
     },
   },
 });
 
-export const {logoutUser ,loginUser, toggleTheme
-} = userSlice.actions
+export const { logoutUser, loginUser, toggleTheme } = userSlice.actions;
 export default userSlice.reducer;
