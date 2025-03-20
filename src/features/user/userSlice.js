6dc57445c31a7c50b7f06e7739d6bf0f4c80cadf
Bina <benjaminbinyam500@gmail.com>
2025-03-20 20:38:10 +0300

    complete login - create loginUser reducer  - localStorage

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,6 +6,10 @@ const themes = {
   dracula: "dracula",
 };
 
+const getUserFromLocalStorage = () => {
+  return JSON.parse(localStorage.getItem("user") || null);
+};
+
 const getThemeFromLocalStorage = () => {
   const theme = localStorage.getItem("theme") || themes.winter;
   document.documentElement.setAttribute("data-theme", theme);
@@ -13,7 +17,7 @@ const getThemeFromLocalStorage = () => {
 };
 
 const initialState = {
-  user: { username: "codding addict" },
+  user: getUserFromLocalStorage(),
   theme: getThemeFromLocalStorage(),
 };
 
@@ -21,8 +25,10 @@ const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    loginUser: (state) => {
-      console.log("login");
+    loginUser: (state, action) => {
+      const user = { ...action.payload.user, token: action.payload.jwt };
+      state.user = user;
+      localStorage.setItem("user", JSON.stringify(user));
     },
     logoutUser: (state) => {
       state.user = null;
