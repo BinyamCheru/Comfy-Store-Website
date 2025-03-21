f6e229b3fd25ccc532a42eec45cd8ca3dc98d6f2
Bina <benjaminbinyam500@gmail.com>
2025-03-21 11:02:21 +0300

    Complete Checkout feature

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,14 +1,14 @@
 const FormInput = ({ label, name, type, defaultValue, size }) => {
   return (
     <div className="form-control ">
-      <label className="label">
+      <label className="label block">
         <span className="label-text capitalize">{label}</span>
       </label>
       <input
         type={type}
         name={name}
         defaultValue={defaultValue}
-        className={`input input-bordered ${size}`}
+        className={`input input-bordered w-full mt-4 ${size}`}
       />
     </div>
   );
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -23,7 +23,7 @@ const Checkout = () => {
   return (
     <>
       <SectionTitle text="Place your order" />
-      <div className="mt-8 grid md:grid-cols-2 items-start">
+      <div className="mt-8 grid gap-8  md:grid-cols-2 items-start">
         <CheckoutForm />
         <CartTotals />
       </div>
