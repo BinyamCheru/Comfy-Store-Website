590a109418dad2120e1e46d2c1f94dfa35da825e
Bina <benjaminbinyam500@gmail.com>
2025-03-21 10:15:29 +0300

    Add checkout page with a warn

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,33 @@
+import { useSelector } from "react-redux";
+import { CartTotals, SectionTitle } from "../components";
+import CheckoutForm from "../components/CheckoutForm";
+import { toast } from "react-toastify";
+import { redirect } from "react-router-dom";
+
+export const loader = (store) => async () => {
+  const user = store.getState().userState.user;
+
+  if (!user) {
+    toast.warn("You must be logged in to checkout");
+    return redirect("/login");
+  }
+  return null;
+};
+
 const Checkout = () => {
-  return <h1 className="text-4xl">Checkout</h1>;
+  const cartTotal = useSelector((state) => state.cartState.cartTotal);
+  if (cartTotal === 0) {
+    return <SectionTitle text="Your cart is empty" />;
+  }
+
+  return (
+    <>
+      <SectionTitle text="Place your order" />
+      <div className="mt-8 grid md:grid-cols-2 items-start">
+        <CheckoutForm />
+        <CartTotals />
+      </div>
+    </>
+  );
 };
 export default Checkout;
